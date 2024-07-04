44d8738fba37058fd58dfe480a954b80591ca934
adityadhamanekar <adityadhamanekar725@gmail.com>
2024-07-04 20:46:21 +0530

    regression complete

---
+++ Part 2 - Regressions/4) Support Vector Regression/svrReg.py
@@ -0,0 +1,34 @@
+
+# Support Vector Regression (SVR)
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
+y = y.reshape(len(y),1)
+print(y)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+print(X)
+print(y)
+
+# Training the SVR model on the whole dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+
+# Predicting a new result
+sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])).reshape(-1,1))
+
---
+++ Part 2 - Regressions/6) Random Forest Regression/rfrReg.py
@@ -0,0 +1,19 @@
+# Random Forest Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+# Training the Random Forest Regression model on the whole dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(X, y)
+
+# Predicting a new result
+regressor.predict([[6.5]])
